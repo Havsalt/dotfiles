624de0fc03eb68abd339cebc3f0aa0bd6b61c7a9
Havsalt <knutolaihelgesen@gmail.com>
2024-10-20 17:22:13 +0200

    Added more colors

--- .powershell/profile.ps1
+++ .powershell/profile.ps1
@@ -1,24 +1,34 @@
 
-function Prompt
-{
+# Custom prompt text
+function Prompt {
     $location = (Get-Location).Path
     $splitLocation = $location -split "\\"
-    $drive = @($splitLocation[0][0])
-    $mappedMiddle =
-        ($splitLocation[1..($splitLocation.Length - 2)]
-            | ForEach-Object { $_[0] })
+    $drive = $splitLocation[0][0]
+    $middleSegments = $splitLocation[1..($splitLocation.Length - 2)]
     $lastSegment = $splitLocation[-1]
-
+    # Write to screen
     Write-Host "Knutern" -NoNewLine -ForegroundColor Yellow
     Write-Host " @ " -NoNewLine -ForegroundColor Magenta
     Write-Host "$drive] " -NoNewLine -ForegroundColor Green
-    Write-Host $($mappedMiddle -join "\") -NoNewLine -ForegroundColor Cyan
-    Write-Host "\" -NoNewLine -ForegroundColor Cyan
+    # Write segments, and color if special
+    foreach ($segment in $middleSegments) {
+        if ($segment -eq "Python") {
+            Write-Host $segment[0] -NoNewLine -ForegroundColor Blue            
+        } elseif ($segment[0] -eq ".") {
+            Write-Host ".$($segment[1])" -NoNewLine -ForegroundColor Magenta            
+        }
+        else {
+            Write-Host $segment[0] -NoNewLine -ForegroundColor Cyan
+        }
+        Write-Host "\" -NoNewLine -ForegroundColor Cyan
+    }
+    # Write last segment
     Write-Host $lastSegment -NoNewLine -ForegroundColor Red
     Write-Host ">" -NoNewLine -ForegroundColor Cyan
     return " "
 }
 
+# `cd` command allowing spaces without double quotes
 # function Set-Location {
 #     param (
 #         [Parameter(ValueFromRemainingArguments)]$segments # Collect all segments

9f2c47ae532abfebd584854dc82cd87179bba987
Havsalt <knutolaihelgesen@gmail.com>
2024-11-01 11:16:21 +0100

    Added support for script block aliases

--- powershell/Aliases.ps1
+++ powershell/Aliases.ps1
@@ -1,7 +1,8 @@
 
-$aliases = @{
-    'lvim' = 'C:\Users\knuto\.local\bin\lvim.ps1'
-    'hr'   = 'here'
+$aliases = @{ # Key: Literal, Value: String | ScriptBlock
+    lvim = 'C:\Users\knuto\.local\bin\lvim.ps1'
+    hr   = 'here'
+    cdf  = { "$(fzf)\.." | cd }
 }
 
 # Display alias count
@@ -11,8 +12,12 @@ Write-Host " aliases" -NoNewLine -ForegroundColor Blue
 Write-Host ": " -NoNewline
 Write-Host "..." -NoNewLine -ForegroundColor Yellow
 # Load each alias
-$aliases.GetEnumerator() | ForEach-Object {
-    Set-Alias -Name $_.key -Value $_.value
+foreach ($alias in $aliases.Keys) {
+    if ($aliases[$alias] -is [string]) {
+        Set-Alias -Name $alias -Value $aliases[$alias]
+    } else { # `$aliases[$alias]` is `[scriptblock]`
+        New-Item -Path "Function:$alias" -Value $aliases[$alias] -ItemType Function -Force | Out-Null
+    }
 }
 # Update loading status to "Done"
 Write-Host "`e[4D Done" -ForegroundColor Green

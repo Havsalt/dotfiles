01b858da63b820cc9f75d91ea5e79b631bac9cea
Havsalt <knutolaihelgesen@gmail.com>
2024-11-30 00:58:50 +0100

    Initial commit

---
+++ powershell/env-vars.ps1
@@ -0,0 +1,34 @@
+# Key: Literal
+# Value: String Path
+$RootRelativeEnvironmentPaths = @{  # Relative to ".dotfiles" project folder
+    XDG_CONFIG_HOME = ".config"
+    CARGO_HOME = ".cargo"
+    RYE_HOME = ".rye"
+}
+$EnvironmentVariables = @{
+    RUST_BACKTRACE = 1
+}
+
+# Calculate total count of environment variables
+$totalCount = (
+    $RootRelativeEnvironmentPaths.Count `
+    + $EnvironmentVariables.Count
+)
+# Display environment variables count and indicate loading of them
+Write-Host "Loading " -NoNewLine
+Write-Host $totalCount -NoNewLine -ForegroundColor Blue
+Write-Host " environment variables" -NoNewLine -ForegroundColor Blue
+Write-Host ": " -NoNewline
+Write-Host "..." -NoNewLine -ForegroundColor Yellow
+# Load each relative environment path variable
+foreach ($relEnvVar in $RootRelativeEnvironmentPaths.Keys) {
+    $joined = "$PSScriptRoot\..\$($RootRelativeEnvironmentPaths[$relEnvVar])"
+    $normalized = [System.IO.Path]::GetFullPath($joined)
+    Set-Variable -Name $relEnvVar -Value $normalized
+}
+# Load generic environment variables
+foreach ($EnvVar in $EnvironmentVariables.Keys) {
+    Set-Variable -Name $EnvVar -Value $EnvironmentVariables[$EnvVar]
+}
+# Update loading status to "Done"
+Write-Host "`e[4D Done" -ForegroundColor Green

a0310a0816d3beb47dc2460bb052998e3e77d730
Havsalt <knutolaihelgesen@gmail.com>
2025-02-17 19:10:19 +0100

    Added alias deep

--- powershell/aliases.ps1
+++ powershell/aliases.ps1
@@ -2,19 +2,18 @@
 # Value: String | ScriptBlock
 $Aliases  = @{
     ls    = 'eza'  # Override default ls
-    lvim  = "$HOME\.local\bin\lvim.ps1"
     hr    = 'here'
     grep  = 'rg'
-    cfmt  = {
-        Write-Host 'Running `cargo fmt`'
-        cargo fmt
-    }
     cdf   = { if ( $filePath = fzf ) { cd "$filePath\.." } }
     codef = {
         $filePath = Resolve-Path "$(fzf)\.." | Select-Object -ExpandProperty Path
         if ( $filePath.Length -gt 3 ) { code "$filePath\.." }  # Longer than "C:\"
     }
     fcode = 'codef'
+    cfmt  = {
+        Write-Host 'Running `cargo fmt`'
+        cargo fmt
+    }
     rr    = {
         Clear-Host         # Clear screen
         Invoke-History -2  # Re-run command
@@ -29,9 +28,13 @@ $Aliases  = @{
     }
 }
 
+function deep {
+    eza.exe -DTL 2  # Help: Read flags as "Directory Tree Levels = 2"
+}
+
 # Display alias count and indicate loading of them
 Write-Host "Loading " -NoNewLine
-Write-Host $Aliases.Count -NoNewLine -ForegroundColor Blue
+Write-Host ($Aliases.Count + 1) -NoNewLine -ForegroundColor Blue  # NOTE: Static +1, because of function `deep`
 Write-Host " aliases" -NoNewLine -ForegroundColor Blue
 Write-Host ": " -NoNewline
 Write-Host "..." -NoNewLine -ForegroundColor Yellow
